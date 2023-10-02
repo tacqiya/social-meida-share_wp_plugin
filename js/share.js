62b577e80cb5ab121c8dfd1f570e832cfdfc577a
typhix-dev <typhixdev@gmail.com>
2023-10-02 09:16:55 +0400

    first commit

---
+++ js/share.js
@@ -0,0 +1,73 @@
+// var pageLink = window.location.href;
+// var pageTitle = String(document.title).replace(/\&/g, '%26');
+
+var pageLink = '';
+var pageTitle = '';
+var details = [];
+
+
+var mainButton = document.getElementsByClassName('main-share-btn'); // Assumes element with id='button'
+var shareList = document.getElementsByClassName('share-list'); // Assumes element with id='button'
+// var clicker = document.getElementsByClassName('main-share-btn');
+// console.log(mainButton)
+
+for (var i = 0; i < mainButton.length; i++) {
+    var div = mainButton[i];
+    var div2 = shareList[i];
+    var dynamicId = (i + 1);
+    var dynamicId2 = "share_list_" + (i + 1);
+    div.setAttribute("id", dynamicId);
+    // div.id = i + 1;
+    div2.setAttribute("id", dynamicId2);
+    // div2.id = i + 1;
+}
+
+
+for (var i = 0; i < mainButton.length; i++) {
+    mainButton[i].onclick = function() {
+        jQuery('.share-list').css('display', 'none');
+        // console.log(this.getAttribute('id'))
+        var id = this.getAttribute('id');
+        var dynamicId = "#share_list_" + (id);
+        jQuery("#share_list_" + id).css('display', 'flex');
+        var div = document.querySelector(dynamicId);
+        // console.log(div)
+        // if (div.style.display === 'none') {
+        //     div.style.display = 'flex';
+        // } else {
+        //     div.style.display = 'none';
+        // }
+        jQuery(dynamicId).toggleClass("flexbox");
+        var otherInput = jQuery(this).closest('.main-share-btn').parent().prev();
+        var fileLink = jQuery(otherInput).attr('href');
+        details = get_share_links(fileLink);
+        // console.log(jQuery(otherInput).attr('href'))
+    };
+    console.log(details);
+}
+
+function get_share_links(links) {
+    var pageLink = links;
+    var pageTitle = 'KU';
+    return [pageLink, pageTitle];
+}
+
+pageLink = details[0];
+pageTitle = details[1];
+
+
+function fbs_click() {
+    console.log(pageLink);
+    window.open(`http://www.facebook.com/sharer.php?u=${details[0]}&quote=${details[1]}`, 'sharer', 'toolbar=0,status=0,width=626,height=436');
+    return false;
+}
+
+function tbs_click() { window.open(`https://twitter.com/intent/tweet?text=${details[1]}&url=${details[0]}`, 'sharer', 'toolbar=0,status=0,width=626,height=436'); return false; }
+
+function lbs_click() { window.open(`https://www.linkedin.com/sharing/share-offsite/?url=${details[0]}`, 'sharer', 'toolbar=0,status=0,width=626,height=436'); return false; }
+
+function rbs_click() { window.open(`https://www.reddit.com/submit?url=${details[0]}`, 'sharer', 'toolbar=0,status=0,width=626,height=436'); return false; }
+
+function pbs_click() { window.open(`https://www.pinterest.com/pin/create/button/?&text=${details[1]}&url=${details[0]}&description=${details[1]}`, 'sharer', 'toolbar=0,status=0,width=626,height=436'); return false; }
+
+function whp_click() { window.open(`https://web.whatsapp.com/send?text=${details[0]}`, 'sharer', 'toolbar=0,status=0,width=626,height=436'); return false; }
---
+++ social-media-share.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Plugin Name: Social Media Share
+ * Description: A simple custom plugin for WordPress.
+ * Version: 1.0
+ * Author: Bohahahaha
+ */
+
+if (!defined('ABSPATH')) {
+    echo "no plugee";
+    exit;
+}
+
+class SocialMediaShare
+{
+    public function __construct()
+    {
+        add_action('init', array($this, 'create_social_media_share'));
+
+        add_action('wp_enqueue_scripts', array($this, 'load_assets'));
+
+        // echo  plugin_dir_url(__FILE__).'js/share.js';
+        add_shortcode('share-buttons', array($this, 'load_shortcode'));
+    }
+
+    public function create_social_media_share()
+    {
+        $args = array(
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array('title'),
+            'exclude_from_search' => true,
+            'capability' => 'manage_options',
+            'labels' => array(
+                'name' => 'Social media share',
+                'singular_name' => 'Social Shares'
+            ),
+            'menu-icon' => 'dashicons-media-text'
+        );
+
+        register_post_type('social_media_share', $args);
+    }
+
+    public function load_assets()
+    {
+        wp_enqueue_style(
+            'social-media-share',
+            plugin_dir_url(__FILE__) . 'css/share.css',
+            array(),
+            1,
+            'all'
+        );
+
+        wp_enqueue_script(
+            'social-media-share',
+            plugin_dir_url(__FILE__) . 'js/share.js',
+            array('jquery'),
+            1,
+            true
+        );
+    }
+
+    public function load_shortcode()
+    { ?>
+        <div class="share-buttons-container">
+            <div class="main-share-btn">
+                <img src="https://cdn-icons-png.flaticon.com/512/61/61020.png">
+            </div>
+            <div class="share-list">
+                <!-- FACEBOOK -->
+                <a class="fb-h" onclick="return fbs_click()" target="_blank">
+                    <img src="https://img.icons8.com/material-rounded/96/000000/facebook-f.png">
+                </a>
+
+                <!-- TWITTER -->
+                <a class="tw-h" onclick="return tbs_click()" target="_blank">
+                    <img src="https://img.icons8.com/material-rounded/96/000000/twitter-squared.png">
+                </a>
+
+                <!-- LINKEDIN -->
+                <a class="li-h" onclick="return lbs_click()" target="_blank">
+                    <img src="https://img.icons8.com/material-rounded/96/000000/linkedin.png">
+                </a>
+
+                <!-- REDDIT -->
+                <a class="re-h" onclick="return rbs_click()" target="_blank">
+                    <img src="https://img.icons8.com/ios-glyphs/90/000000/reddit.png">
+                </a>
+
+                <!-- PINTEREST -->
+                <a data-pin-do="buttonPin" data-pin-config="none" class="pi-h" onclick="return pbs_click()" target="_blank">
+                    <img src="https://img.icons8.com/ios-glyphs/90/000000/pinterest.png">
+                </a>
+
+                <!-- WHATSAPP -->
+                <a data-action="share/whatsapp/share" class="wh-h" onclick="return whp_click()" target="_blank">
+                    <img src="https://img.icons8.com/ios-glyphs/90/000000/whatsapp.png">
+                </a>
+
+                <!-- MAIL -->
+                <a data-action="share/whatsapp/share" class="ma-h" onclick="return whp_click()" target="_blank">
+                    <img src="https://img.icons8.com/ios-glyphs/90/000000/gmail.png">
+                </a>
+
+                <!-- INSTAGRAM -->
+                <a data-action="share/whatsapp/share" class="in-h" onclick="return whp_click()" target="_blank">
+                    <img src="https://img.icons8.com/ios-glyphs/90/000000/instagram.png">
+                </a>
+
+                <!-- GMAIL -->
+                <a data-action="share/whatsapp/share" class="gm-h" onclick="return whp_click()" target="_blank">
+                    <img src="https://img.icons8.com/ios-glyphs/90/000000/gmail.png">
+                </a>
+
+                <!-- Thread -->
+                <a data-action="share/whatsapp/share" class="gm-h" onclick="return whp_click()" target="_blank">
+                    <img src="https://img.icons8.com/ios-glyphs/90/000000/gmail.png">
+                </a>
+            </div>
+        </div>
+<?php }
+}
+
+new SocialMediaShare;
